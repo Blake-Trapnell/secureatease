d0c403a4ef115f01d03c60311913b76bd031ef62
Blake Trapnell <blake.trapnell103@gmail.com>
2021-07-25 13:26:23 -0600

    added owned photos

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -9,14 +9,15 @@ const Landing = () => {
 					<div className={"landing--home-introduction-section"}>
 						<section className={"landing--text-section"}>
 							<h1 className={"landing--main-text"}>
-								Secure@ease
+								Secure@Ease
 							</h1>
 							<h2 className={"landing--secondary-text"}>
-								Home solutions
+								Managment
 							</h2>
 						</section>
 					</div>
 						<Cards/>
+			<div className={"about"}></div>
 				</div>
 			</div>
 		</section>
--- src/components/nav/navbar.js
+++ src/components/nav/navbar.js
@@ -1,13 +1,24 @@
 import "./navbar.scss";
 
 const Navbar = () => {
-    return (
-        <nav className={"navbar--outer-container"}>
-            <h3 className={"navbar--location"}>Home</h3>
-            <h3 className={"navbar--location"}>Something</h3>
-            <h3 className={"navbar--location"}>Something Else</h3>
-        </nav>
-    )
-}
+	return (
+		<nav className={"navbar--outer-container"}>
+			<div className="navbar--navigation-section">
+				<h3 className={"navbar--location"}>Secure@ease</h3>
+				<h3 className={"navbar--location"}>Rentals</h3>
+				<h3 className={"navbar--location"}>Design</h3>
+				<h3 className={"navbar--location"}>Cleaning Services</h3>
+				<h3 className={"navbar--location"}>Contact</h3>
+			</div>
+			<div className={"navbar--info-section"}>
+				{/* <div className={"navbar--login"}>
+					<div className="navbar--login-logo" />
+					<h3 className={"navbar--location"}>login</h3>
+				</div> */}
+				<h2 className="navbar--location">123-123-1234</h2>
+			</div>
+		</nav>
+	);
+};
 
 export default Navbar;
