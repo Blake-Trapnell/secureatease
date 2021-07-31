9d9957e16cf3481e0506d4999b22ac0828874e0d
Blake Trapnell <blake.trapnell103@gmail.com>
2021-07-31 15:52:11 -0600

    for sale section basics added

--- src/components/forSale/forSale.js
+++ src/components/forSale/forSale.js
@@ -1,11 +1,36 @@
 import "./forSale.scss";
 
-// import MapletonImages from "./forSaleImages.js";
+import MapletonImages from "./forSaleImages.js";
+import ForSaleData from "./forSaleData.js";
 
 const ForSale = () => {
 	return (
-        <div>
-        </div>
+		<div className={"for-sale--outer-container"}>
+			{ForSaleData.map((el, i) => {
+				return (
+					<div
+						key={el.key}
+						className={"for-sale--listing-outer-container"}
+					>
+						<div className={"for-sale--listing-container"}>
+                            <div>
+                                <h3 className={"for-sale--listing-info"}>{el.address}</h3>
+                                <h3 className={"for-sale--listing-info"}>{el.city}</h3>
+                                <h3 className={"for-sale--listing-info"}>{el.state}</h3>
+                                <h3 className={"for-sale--listing-info"}>{el.zip}</h3>
+
+                            </div>
+                            <div className={"for-sale--listing-contact-container"}>
+                                <h3 className={"for-sale--listing-info"}>{el.contactName}</h3>
+                                <h3 className={"for-sale--listing-info for-sale--listing-contact"}>{el.contactPhone}</h3>
+                                <h3 className={"for-sale--listing-info for-sale--listing-contact"}>{el.contactEmail}</h3>
+                            </div>
+                        </div>
+                        <img className={"for-sale--listing-background"} src={MapletonImages[i]} alt="" />
+					</div>
+				);
+			})}
+		</div>
 	);
 };
 
--- src/components/forSale/forSaleData.js
+++ src/components/forSale/forSaleData.js
@@ -1,15 +1,19 @@
-
-
 const ForSaleData = [
     {
-        homeName: "",
-        address: "",
+        homeName: "Mapleton White House",
+        address: "1134 E 800 N",
+        city: "Mapleton",
+        state: "Utah",
+        zip: "84664",
         backgroundPictures: [],
         listingPrice: "",
-        dateListed: "",
-        contactName: "",
-        contactPhone: "",
-        contactEmail: "",
+        dateListed: "8/1/2021",
+        contactName: "Susan",
+        contactPhone: "801-989-4504",
+        contactEmail: "secure.at.ease.management@gmail.com",
         sold: false,
-    }
-]
+        key: "121"
+    },
+];
+
+export default ForSaleData;
