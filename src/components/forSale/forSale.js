3921f78303d5b56cebc21e0241bcaac51995570e
Blake Trapnell <blake.trapnell103@gmail.com>
2021-07-31 18:50:11 -0600

    for-sale listing style changes

--- src/components/forSale/forSale.js
+++ src/components/forSale/forSale.js
@@ -13,20 +13,52 @@ const ForSale = () => {
 						className={"for-sale--listing-outer-container"}
 					>
 						<div className={"for-sale--listing-container"}>
-                            <div>
-                                <h3 className={"for-sale--listing-info"}>{el.address}</h3>
-                                <h3 className={"for-sale--listing-info"}>{el.city}</h3>
-                                <h3 className={"for-sale--listing-info"}>{el.state}</h3>
-                                <h3 className={"for-sale--listing-info"}>{el.zip}</h3>
-
-                            </div>
-                            <div className={"for-sale--listing-contact-container"}>
-                                <h3 className={"for-sale--listing-info"}>{el.contactName}</h3>
-                                <h3 className={"for-sale--listing-info for-sale--listing-contact"}>{el.contactPhone}</h3>
-                                <h3 className={"for-sale--listing-info for-sale--listing-contact"}>{el.contactEmail}</h3>
-                            </div>
-                        </div>
-                        <img className={"for-sale--listing-background"} src={MapletonImages[i]} alt="" />
+							<div className={"for-sale--listing-info-container"}>
+								<h3 className={"for-sale--listing-info"}>
+									{el.address}
+								</h3>
+								<h3 className={"for-sale--listing-info"}>
+									{el.city}
+								</h3>
+								<h3 className={"for-sale--listing-info"}>
+									{el.state}
+								</h3>
+								<h3 className={"for-sale--listing-info"}>
+									{el.zip}
+								</h3>
+							</div>
+							<div
+								className={
+									"for-sale--listing-contact-container"
+								}
+							>
+								<h3 className={"for-sale--listing-info"}>
+									{el.contactName}
+								</h3>
+								<h3
+									className={
+										"for-sale--listing-info for-sale--listing-contact"
+									}
+								>
+									{el.contactPhone}
+								</h3>
+								<h3
+									className={
+										"for-sale--listing-info for-sale--listing-contact"
+									}
+								>
+									{el.contactEmail}
+								</h3>
+							</div>
+						</div>
+						<div
+							className={"for-sale--listing-background"}
+							style={{ background: `url(${MapletonImages[i]})`, backgroundSize: "cover" }}
+						>
+							<button className="for-sale--action-button action--button">
+								View Listing
+							</button>
+						</div>
 					</div>
 				);
 			})}
